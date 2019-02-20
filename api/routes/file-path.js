ef231681bd9a417de1f67af87d6705fee0a60e96
artem-trachuk <trachuk.artem@gmail.com>
2019-02-20 14:33:29 +0200

    update

--- api/routes/file-path.js
+++
@@ -1,8 +0,0 @@
-module.exports.path = {
-  logo: "uploads/logo/",
-  relativeLogo: "uploads/logo/",
-  photos: "uploads/photos/",
-  relativePhotos: "uploads/photos/",
-  documents: "uploads/documents/",
-  relativeDocuments: "uploads/documents/",
-};
